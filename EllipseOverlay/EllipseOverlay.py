d273be918353ef62d9d1efda6e21ed512885a9b9
Aranya Kumar <axk1067@case.edu>
2020-06-26 15:59:28 -0400

    Detects existing ellipses in the granite

--- EllipseOverlay/EllipseOverlay.py
+++ EllipseOverlay/EllipseOverlay.py
@@ -10,6 +10,38 @@ import numpy as np
 
 image = cv2.imread('granite.jpg', cv2.IMREAD_COLOR) #read the RGB image
 class EllipseOverlay:
+
+    def detect():
+        params = cv2.SimpleBlobDetector_Params()
+
+        params.filterByArea = True
+        params.minArea = 10
+
+        params.filterByCircularity = True 
+        params.minCircularity = 0.7
+
+        params.filterByConvexity = True
+        params.minConvexity = 0.5
+
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.01
+
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        keypoints = detector.detect(image)
+
+        blank = np.zeros((1, 1))  
+        blobs = cv2.drawKeypoints(image, keypoints, blank, (0, 0, 255), 
+                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
+
+        number_of_blobs = len(keypoints) 
+        text = "Number of Circular Blobs: " + str(len(keypoints)) 
+        cv2.putText(blobs, text, (20, 550), 
+        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2) 
+
+        cv2.imshow("Filtering Circular Blobs Only", blobs) 
+        cv2.waitKey(0) 
+        cv2.destroyAllWindows() 
      
     def drawEllipse():
         ellipse = cv2.ellipse(image,(256,256),(100,50),0,0,180,255,-1)
@@ -17,5 +49,10 @@ class EllipseOverlay:
         cv2.waitkey(0) #waits for the key press
 
     def main():
+        detect()
+        drawEllipse()
+
+    if __name__ == '__main__':
+        detect()
         drawEllipse()
 
