a2053dd0e87a8e18a7643ed5c033968244e9db17
Aranya Kumar <axk1067@case.edu>
2020-08-09 21:20:14 -0400

    Overlays ellipse on given image

--- EllipseOverlay.py
+++ EllipseOverlay.py
@@ -1,69 +1,71 @@
-"""
-   This class creates false ellipses
-    in granite images to test the users'
-    ability to detect ellipses 
-"""
-        
-#importing OpenCV
-import cv2
+# importing cv2 
+import cv2 
+import argparse
+import random
 import numpy as np
-import matplotlib.patches as mpatches
-from PIL import Image
+from matplotlib import pyplot as plt
+from skimage.color import rgb2lab, deltaE_cie76
+import image_slicer
 
-im = Image.open("granite.jpg") #read the RGB image
-class EllipseOverlay:
+# Cut image into 4 subsections
+#im = cv2.imread('graniteSlab.jpeg')
+tiles = image_slicer.slice('graniteSlab.jpeg', 4)
 
-    def detect():
-        params = cv2.SimpleBlobDetector_Params()
+# Path
+im = cv2.imread("graniteSlab_02_02.png")
 
-        params.filterByArea = True
-        params.minArea = 10
+# Construct arg parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-r", "--radius", type = int,
+	help = "radius of Gaussian blur; must be odd")
+args = vars(ap.parse_args())
+    
+# Reading an image in default mode getting width and height
+image = im
+(width, height, channel) = image.shape  
 
-        params.filterByCircularity = True 
-        params.minCircularity = 0.7
+### Convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)               #convert to gray
+### Apply a Gaussian blur to the image
+gray = cv2.blur(gray, (10, 10))
+### Find the darkest and brightest region
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+    
+# Window name in which image is displayed 
+window_name = 'Image'
+   
+center_coordinates = (random.randint(10, width), random.randint(10, height)) 
+  
+axesLength = (15, 10) 
+  
+angle = random.randint(0,360)
+  
+startAngle = 0
+  
+endAngle = 360
+   
+# Gets darkest color in the image 
+color = (minVal, minVal, minVal) 
+   
+# Line thickness of -1 px 
+thickness = -1
+   
+# Using cv2.ellipse() method 
+# Draw a ellipse with blue line borders of thickness of -1 px 
+image = cv2.ellipse(image, center_coordinates, axesLength, angle, 
+                          startAngle, endAngle, color, -1) 
 
-        params.filterByConvexity = True
-        params.minConvexity = 0.5
-
-        params.filterByInertia = True
-        params.minInertiaRatio = 0.01
-
-        detector = cv2.SimpleBlobDetector_create(params)
-
-        keypoints = detector.detect(im);
-
-        blank = np.zeros((1, 1))  
-        blobs = cv2.drawKeypoints(im, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
-
-        number_of_blobs = len(keypoints) 
-        text = "Number of Circular Blobs: " + str(len(keypoints)) 
-        cv2.putText(blobs, text, (20, 550), 
-        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2) 
-
-        cv2.imshow("Filtering Circular Blobs Only", blobs) 
-        cv2.waitKey(0) 
-        cv2.destroyAllWindows() 
-     
-    def drawEllipse():
-        ellipse = cv2.ellipse(im,(256,256),(100,50),0,0,180,(255, 0, 0),5)
-        im.show() #show the image
-        cv2.waitkey(0) #waits for the key press
-
-    def ellipseInfo(filename):
-        with open(filename, 'w') as csvfile:  
-        # creating a csv writer object  
-        csvwriter = csv.writer(csvfile)  
-        
-        # writing the fields  
-        csvwriter.writerow(fields)  
-        
-        # writing the data rows  
-        csvwriter.writerows(rows) #create rows of the information needed
-
-    def main():
-        detect()
-        drawEllipse()
-
-    if __name__ == '__main__':
-        main()
-
+# Creating noise
+noise = np.random.normal(1000., 1000., (1000, 1000, 3))
+granite = cv2.imwrite("gaussian_noise.png", noise)
+   
+# Displaying the image 
+print(minVal)
+print(maxVal)
+cv2.waitKey(0)
+cv2.imshow("Blur", gray)
+cv2.imshow('False Ellipse', image)
+cv2.waitKey(0)
+cv2.imwrite("granite_04_fake.png", image)
+cv2.destroyAllWindows()
